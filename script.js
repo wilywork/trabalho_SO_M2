030c3324d80021bec6437adfdcb385769768d413
WilyWork <hezodions@gmail.com>
2023-10-30 17:56:33 -0300

    rename var; report update;

--- script.js
+++ script.js
@@ -22,22 +22,22 @@ function main() {
     let tamanhoDaPagina = argumentosPersonalizados.deslocamento || 4096;
     let usaHierarquia = Boolean(argumentosPersonalizados.hierarquia);
     let arquivo = argumentosPersonalizados.arquivo;
-    let numeros_decimais;
+    let listaDeEnderecos;
 
     listaDosEnderecosLocalizados = [];
 
     if (arquivo) {
         const file = fs.readFileSync(arquivo, 'utf-8');
-        numeros_decimais = file.replace(/\r/g, '').split('\n');
+        listaDeEnderecos = file.replace(/\r/g, '').split('\n');
     } else {
-        numeros_decimais = [enderecoSolicitado];
+        listaDeEnderecos = [enderecoSolicitado];
     }
 
-    for (const endereco of numeros_decimais) {
+    for (const endereco of listaDeEnderecos) {
         if (endereco != '') {
-            let eh32bits = Boolean(enderecoSolicitado > 65535);
+            let eh32bits = Boolean(endereco > 65535);
             let localizado = null;
-            if (eh32bits && usaHierarquia) {
+            if (usaHierarquia) {
                 localizado = localizarEnderecoHierarquia(endereco, tamanhoDaPagina, eh32bits);
             } else {
                 localizado = localizarEndereco(endereco, tamanhoDaPagina, eh32bits);
@@ -80,14 +80,14 @@ function localizarEnderecoHierarquia(enderecoSolicitado, tamanhoDaPagina, eh32bi
 
     tamanhoDaPagina = 4096; // mantem o tamanho de 4Kb conforme solicitado
 
-    let enderecoBinario = decimalToBinary(enderecoSolicitado).padStart(32, 0);
+    let enderecoBinario = decimalToBinary(enderecoSolicitado).padStart(32, 0); // fixado 32bits
     let limitMaximoHierarquia = 1024;
     let numero_paginas_nivel1 = 0; // limit maximo de 1024 = 10 bits
     let numero_paginas_nivel2 = 0; // limit maximo de 1024 = 10 bits
     let numero_deslocamento = 0;
 
-    let bitsParaDeslocamento = eh32bits ? 12 : Math.log2(tamanhoDaPagina);
-    let bitsParaNiveis = eh32bits ? 10 : 0;
+    let bitsParaDeslocamento = Math.log2(tamanhoDaPagina); // 12
+    let bitsParaNiveis = 10; // conforme solicitado
 
     for (const numero of numeros_decimais) {
         let binarioNivel1 = decimalToBinary(numero_paginas_nivel1).padStart(bitsParaNiveis, 0);
@@ -97,24 +97,21 @@ function localizarEnderecoHierarquia(enderecoSolicitado, tamanhoDaPagina, eh32bi
         let binarioCompleto = binarioNivel1 + binarioNivel2 + binarioDeslocamento;
 
         if (enderecoBinario == binarioCompleto) {
-            obj = {
+            return {
                 enderecoSolicitado,
                 numero_paginas_nivel1,
-                numero_paginas_nivel2
+                numero_paginas_nivel2,
+                numero_deslocamento,
+                valorLido: numero
             };
-
-            obj.numero_deslocamento = numero_deslocamento;
-            obj.valorLido = numero;
-
-            return obj;
         }
 
         numero_deslocamento++;
 
-        if (numero_deslocamento >= tamanhoDaPagina) {
+        if (numero_deslocamento >= tamanhoDaPagina) { // 4096
             numero_deslocamento = 0;
             numero_paginas_nivel2++;
-            if (numero_paginas_nivel2 >= limitMaximoHierarquia) {
+            if (numero_paginas_nivel2 >= limitMaximoHierarquia) { // 1024
                 numero_paginas_nivel2 = 0;
                 numero_paginas_nivel1++;
             }
@@ -128,7 +125,7 @@ function localizarEndereco(enderecoSolicitado, tamanhoDaPagina, eh32bits) {
 
     let numero_paginas = 0;
     let numero_deslocamento = 0;
-    let tratamentoBinarioDeslocamento = Math.log2(tamanhoDaPagina);
+    let tratamentoBinarioDeslocamento = Math.log2(tamanhoDaPagina); // 256b 512b 1024b 2048b 4096b
 
     for (const numero of numeros_decimais) {
         var binario = '';
